a51c4b1d959de9f2f41cf88627b451de4e085273
Antonio Falcescu <79379869+Rodioo@users.noreply.github.com>
2024-01-07 08:27:48 +0200

    added 21 Merge Two Sorted Lists

---
+++ Linked List/merge-two-sorted-lists-21.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/merge-two-sorted-lists/
+
+# TL;DR:
+##  We'll create a new linked list head and use a tail starting from that head to iterate through the list
+##  Iterate through our initial 2 lists until one of them becomes null:
+##      - for each current node of our 2 lists check which is lower and assign it to the tail of the newly created linked list
+##  After one of the lists becomes null check if the other list still has nodes and append all of them to the tail
+##  Return the head.next because head is still null (the way we append nodes to the tail is always by calling tail.next = node so that's why the first node(head) remains null)
+
+# Complexities:
+## Time => O(n), where n is the length of the linked list
+## Space => O(1)
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = ListNode()
+        tail = head
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+
+        tail.next = list1 or list2
+
+        return head.next
