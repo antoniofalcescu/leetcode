5b5c7dc0bc11fc90067f602ce4bf6f12714185fb
antoniofalcescu <antonio.falcescu@gmail.com>
2025-11-11 10:34:15 +0200

    updated Diameter of Binary Tree 543

--- Trees/diameter-of-binary-tree-541.py
+++ Trees/diameter-of-binary-tree-541.py
@@ -2,15 +2,15 @@
 
 # TL;DR:
 ##  We'll solve the problem recursively with a DFS Postorder approach using a helper function
-##  For each node starting with root check if it's null (stop case for the recursive algorithm) and return -1 which is the height of that node
+##  For each node starting with root check if it's null (stop case for the recursive algorithm) and return 0 representing the diameter of the node
 ##  With the Postorder approach we have to iterate through the left subtree, then the right and then calculate the values from the root
-##  After visiting both subtrees, update the max_diam variable with the left_height + right_height + 2 (use 2 to counter the -1 for null subtrees)
-##  Update the height of the current subtree from the DFS by returning the max height from either left or right subtrees and add 1 to it
+##  After visiting both subtrees, update the max_diam variable with the left diameter + right diameter
+##  Update the diameter of the current subtree from the DFS by returning the max diameter from either left or right subtrees and add 1 to it
 ##  Call the helper function and return the value stored inside the max_diam variable
 
 # Complexities:
 ## Time => O(n), where n is the number of nodes in the tree
-## Space => O(1)
+## Space => O(h), where h is the height of the tree
 
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
