2e6935f4562dde05179d206e3902f8ce19bd1f51
antoniofalcescu <antonio.falcescu@gmail.com>
2025-11-24 15:53:32 +0200

    updated Search a 2D Matrix 74

--- Binary Search/search-a-2d-matrix-74.ts
+++ Binary Search/search-a-2d-matrix-74.ts
@@ -11,42 +11,39 @@
 // Space => O(1)
 
 function searchMatrix(matrix: number[][], target: number): boolean {
-	let top = 0;
-	let bottom = matrix.length - 1;
-
+	const ROWS = matrix.length;
+	const COLS = matrix[0].length;
+	
+	let row = -1;
+	let [top, bottom] = [0, ROWS - 1];
 	while (top <= bottom) {
-		const mid = Math.floor((bottom + top) / 2);
-
-		if (target > matrix[mid][matrix[mid].length - 1]) {
-			top = mid + 1;
-		} else if (target < matrix[mid][0]) {
-			bottom = mid - 1;
-		} else {
-			top = mid;
-			break;
-		}
+			const mid = Math.floor((top + bottom) / 2);
+			if (matrix[mid][0] <= target && matrix[mid][COLS - 1] >= target) {
+					row = mid;
+					break;
+			}
+			if (matrix[mid][0] > target) {
+					bottom = mid - 1;
+			} else if (matrix[mid][COLS - 1] < target) {
+					top = mid + 1;
+			}
+	}
+	if (row === -1) {
+			return false;
 	}
 
-	if (top >= matrix.length) {
-		return false;
-	}
-
-	let left = 0;
-	let right = matrix[top].length - 1;
-
+	let [left, right] = [0, COLS - 1];
 	while (left <= right) {
-		const mid = Math.floor((right + left) / 2);
-
-		if (matrix[top][mid] === target) {
-			return true;
-		}
-
-		if (matrix[top][mid] > target) {
-			right = mid - 1;
-		} else {
-			left = mid + 1;
-		}
+			const mid = Math.floor((left + right) / 2);
+			if (matrix[row][mid] === target) {
+					return true;
+			}
+			if (matrix[row][mid] > target) {
+					right = mid - 1;
+			} else {
+					left = mid + 1;
+			}
 	}
 
 	return false;
-}
+};
