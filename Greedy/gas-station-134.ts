bbe97f24fc800c07ac80b0ab54bcfcda082fa66e
antoniofalcescu <antonio.falcescu@gmail.com>
2025-11-25 11:12:58 +0200

    updated Gas Station 134

--- Greedy/gas-station-134.ts
+++ Greedy/gas-station-134.ts
@@ -3,31 +3,31 @@
 // TL;DR:
 // Use a greedy approach
 //   - First check if the total gas is less than the total cost, if so, return -1 (impossible to complete the circuit)
-//   - Initialize a variable for the answer index and a total variable
-//   - Iterate through the gas and cost arrays and update the total diff (gas - cost)
-//   - If the total is less than 0, reset the total to 0 and update the answer to the next index (greedy choice - current subarray is not helpful for future elements)
-//   - This diff total works because we are guaranteed to have a solution (total gas >= total cost)
+//   - Initialize a variable for the answer index and 2 sum variables (one for the current sum and one for the max sum)
+//   - Iterate backwards through the gas and cost arrays and update the current sum with the diff
+//   - If the current sum is greater than the max sum, update the max sum and the answer index
+//   - Return the answer index
 
 // Complexities:
 // Time => O(n), where n is the length of the input arrays
 // Space => O(1)
 
 function canCompleteCircuit(gas: number[], cost: number[]): number {
-	const gasSum = gas.reduce((a, b) => a + b, 0);
-	const costSum = cost.reduce((a, b) => a + b, 0);
-	if (gasSum < costSum) {
+	const gasSum = gas.reduce((acc, x) => acc + x, 0);
+	const costSum = cost.reduce((acc, x) => acc + x, 0);
+
+	if (costSum > gasSum) {
 		return -1;
 	}
 
-	let total = 0;
-	let ans = 0;
-	for (let i = 0; i < gas.length; i++) {
-		total += gas[i] - cost[i];
-		if (total < 0) {
-			total = 0;
-			ans = i + 1;
+	let ans = -1;
+	let [sum, maxSum] = [0, -1];
+	for (let i = gas.length - 1; i >= 0; i--) {
+		sum += gas[i] - cost[i];
+		if (sum > maxSum) {
+			maxSum = sum;
+			ans = i;
 		}
 	}
-
 	return ans;
 }
