5aa392bfd2893997217e8a837f0fa8559934642c
antoniofalcescu <antonio.falcescu@gmail.com>
2025-11-25 11:47:31 +0200

    updated Car Fleet 853

--- Stack/car-fleet-853.ts
+++ Stack/car-fleet-853.ts
@@ -1,12 +1,11 @@
 // https://leetcode.com/problems/car-fleet/
 
 // TL;DR:
-// Use a monotonic increasing stack to keep track of the time it takes for each car to reach the target
+// Use a monotonic strictly increasing stack to keep track of the time it takes for each car to reach the target
 // Sort the pair of position and speed in descending order (by position)
 // Iterate through the sorted pairs and:
 //  - Calculate the time it takes for the current car to reach the target
-//  - Push the time to the stack
-//  - If the current time is <= the time of the previous car in the stack, pop the stack (they will form a fleet)
+//  - Push the time to the stack only if the current time is greater than the time of the previous car in the stack
 // Return the length of the stack
 
 // Complexities:
@@ -14,17 +13,14 @@
 // Space => O(n), where n is the length of the input arrays
 
 function carFleet(target: number, position: number[], speed: number[]): number {
+	const pairs = position.map((pos, i) => [pos, speed[i]]);
+	pairs.sort((a, b) => b[0] - a[0]);
 	const stack: number[] = [];
-	const posSpeedPairs = position.map((pos, i) => [pos, speed[i]]);
-	posSpeedPairs.sort((a, b) => b[0] - a[0]);
-	for (const [pos, speed] of posSpeedPairs) {
+	for (const [pos, speed] of pairs) {
 		const time = (target - pos) / speed;
-		stack.push(time);
-		const stkLength = stack.length;
-		if (stkLength > 1 && stack[stkLength - 1] <= stack[stkLength - 2]) {
-			stack.pop();
+		if (!stack.length || stack[stack.length - 1] < time) {
+			stack.push(time);
 		}
 	}
-
 	return stack.length;
 }
