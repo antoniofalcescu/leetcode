bd0550a57ae34a5e631e134e395ef3c70b972956
antoniofalcescu <antonio.falcescu@gmail.com>
2025-11-26 17:56:22 +0200

    updated Construct BInary Tree from Preorder and Inorder Traversal 105

--- Trees/construct-binary-tree-from-preorder-and-inorder-traversal-105.ts
+++ Trees/construct-binary-tree-from-preorder-and-inorder-traversal-105.ts
@@ -1,14 +1,14 @@
 // https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
 
 // TL;DR:
-// Use a recursive DFS approach to build the tree
+// Use a recursive DFS approach
 // The algorithm works on the following logic:
 //   - The first element of the preorder array is always the root of the current subtree
 //   - The index of the current element in the inorder array is the separator between the left and right children subtrees
 // Initialize a map indices for the inorder array so that we can get the index of the current element in O(1) time
-// Run the DFS in a binary search manner by using left and right pointers to split the inorder
+// Run the DFS by using left and right pointers to split the inorder array based on the root value (first element of the preorder array)
 //   - Base case is left > right which means we've reached the end of the current subtree
-//   - Otherwise, we create the current root TreeNode, increment the preorder array index and use the inorder indices to get the inorder array index
+//   - Otherwise, we create the current root TreeNode, increment the preorder array index and use the inorder indices map to get the inorder array index
 //   - Recursively call the DFS for the left and right subtrees based on the inorder index
 
 // Complexities:
@@ -17,20 +17,23 @@
 
 function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
 	let preorderIdx = 0;
-	const inorderIndices = {};
+	const inorderIndices = new Map<number, number>();
 	for (let i = 0; i < inorder.length; i++) {
-		inorderIndices[inorder[i]] = i;
+		inorderIndices.set(inorder[i], i);
 	}
 
-	function dfs(left, right) {
+	function dfs(left: number, right: number): TreeNode | null {
 		if (left > right) {
 			return null;
 		}
 
-		const rootValue = preorder[preorderIdx++];
-		const root = new TreeNode(rootValue);
-		root.left = dfs(left, inorderIndices[rootValue] - 1);
-		root.right = dfs(inorderIndices[rootValue] + 1, right);
+		const rootVal = preorder[preorderIdx];
+		const root = new TreeNode(rootVal);
+
+		preorderIdx++;
+		const inorderIdx = inorderIndices.get(rootVal)!;
+		root.left = dfs(left, inorderIdx - 1);
+		root.right = dfs(inorderIdx + 1, right);
 
 		return root;
 	}
