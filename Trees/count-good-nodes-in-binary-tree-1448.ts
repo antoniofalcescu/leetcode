51ab60e0bc83691219504ff2d0e6f3024f0b6f4d
antoniofalcescu <antonio.falcescu@gmail.com>
2025-11-26 15:30:28 +0200

    updated Count Good Nodes in Binary Tree 1448

--- Trees/count-good-nodes-in-binary-tree-1448.ts
+++ Trees/count-good-nodes-in-binary-tree-1448.ts
@@ -1,46 +1,31 @@
 // https://leetcode.com/problems/count-good-nodes-in-binary-tree/
 
 // TL;DR:
-// Use a BFS approach to traverse the tree level by level
-// For each node, we also keep track of the max value found so far in the tree from root downwards per level
-// If the current node's value is greater than or equal to the max value found so far, we increment the answer
-// We return the answer
+// Use a DFS approach
+// For each node, if its value is >= max found so far (top-bottom), we increment the answer
+// Then, we call the DFS for the left and right children with the new max value (max(max, current node's value))
+// We return the answer after the DFS call
 
 // Complexities:
 // Time => O(n), where n is the number of nodes in the tree
 // Space => O(n), where n is the number of nodes in the tree
 
 function goodNodes(root: TreeNode | null): number {
-	if (!root) {
-		return 0;
-	}
-
-	const queue = [root];
-	const maxQueue: number[] = [root.val];
 	let ans = 0;
 
-	while (queue.length) {
-		const top = queue.shift();
-		const max = maxQueue.shift() as number;
-
-		if (top) {
-			if (top.left) {
-				queue.push(top.left);
-				const maxLeft = Math.max(max, top.left.val);
-				maxQueue.push(maxLeft);
-			}
-
-			if (top.right) {
-				queue.push(top.right);
-				const maxRight = Math.max(max, top.right.val);
-				maxQueue.push(maxRight);
-			}
-
-			if (top.val >= max) {
-				ans++;
-			}
+	function dfs(node: TreeNode | null, max: number): void {
+		if (!node) {
+			return;
 		}
+
+		if (node.val >= max) {
+			ans++;
+		}
+
+		dfs(node.left, Math.max(max, node.val));
+		dfs(node.right, Math.max(max, node.val));
 	}
 
+	dfs(root, -Infinity);
 	return ans;
 }
