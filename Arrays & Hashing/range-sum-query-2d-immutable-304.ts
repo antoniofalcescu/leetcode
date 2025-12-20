97b5da68d82e4ba1da9e614c72762ab58ad02171
antoniofalcescu <antonio.falcescu@gmail.com>
2025-12-20 08:13:54 +0200

    updated Range Sum Query 2D Immutable 304

--- Arrays & Hashing/range-sum-query-2d-immutable-304.ts
+++ Arrays & Hashing/range-sum-query-2d-immutable-304.ts
@@ -6,9 +6,9 @@
 // TL;DR:
 // Use a prefix sum matrix to store the sum of the submatrixes
 // In the constructor, iterate through the matrix and calculate the prefix sum for each cell (border top and left with 0 for ease of use)
-//   - Formula is: prefix[i + 1][j + 1] = prefix[i][j + 1] + prefix[i + 1][j] + matrix[i][j] - prefix[i][j]
+//   - Formula is: prefix[i][j] = left + top + matrix[i][j] - topLeft
 // For the sumRegion function, use the prefix sum matrix to calculate the sum of the submatrix in O(1)
-//   - Formula is: sum = prefix[row2 + 1][col2 + 1] - prefix[row1][col2 + 1] - prefix[row2 + 1][col1] + prefix[row1][col1]
+//   - Formula is: sum = bottomRight - outsideTopRight - outsideBottomLeft + outsideTopLeft
 
 // Complexities:
 // Time => O(1) for the sumRegion function, O(m * n) for the constructor
@@ -21,25 +21,26 @@ class NumMatrix {
 		const ROWS = matrix.length;
 		const COLS = matrix[0].length;
 
-		this.prefix = Array.from({ length: ROWS + 1 }, () =>
-			Array.from({ length: COLS + 1 }, () => 0)
+		this.prefix = Array.from({ length: ROWS }, () =>
+			Array.from({ length: COLS }, () => 0)
 		);
 		for (let i = 0; i < ROWS; i++) {
 			for (let j = 0; j < COLS; j++) {
-				this.prefix[i + 1][j + 1] =
-					this.prefix[i][j + 1] +
-					this.prefix[i + 1][j] +
-					matrix[i][j] -
-					this.prefix[i][j];
+				const left = j > 0 ? this.prefix[i][j - 1] : 0;
+				const top = i > 0 ? this.prefix[i - 1][j] : 0;
+				const topLeft = i * j > 0 ? this.prefix[i - 1][j - 1] : 0;
+
+				this.prefix[i][j] = matrix[i][j] + top + left - topLeft;
 			}
 		}
 	}
 
 	sumRegion(row1: number, col1: number, row2: number, col2: number): number {
-		const bottomRight = this.prefix[row2 + 1][col2 + 1];
-		const outsideTopRight = this.prefix[row1][col2 + 1];
-		const outsideBottomLeft = this.prefix[row2 + 1][col1];
-		const outsideTopLeft = this.prefix[row1][col1];
+		const bottomRight = this.prefix[row2][col2];
+		const outsideTopRight = row1 > 0 ? this.prefix[row1 - 1][col2] : 0;
+		const outsideBottomLeft = col1 > 0 ? this.prefix[row2][col1 - 1] : 0;
+		const outsideTopLeft =
+			row1 * col1 > 0 ? this.prefix[row1 - 1][col1 - 1] : 0;
 
 		return bottomRight - outsideTopRight - outsideBottomLeft + outsideTopLeft;
 	}
