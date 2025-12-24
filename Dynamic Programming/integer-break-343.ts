d3a7ad33ded95178c9fbad8f655b716e1e29e5da
antoniofalcescu <antonio.falcescu@gmail.com>
2025-12-24 08:41:03 +0200

    updated Integer Break 343

--- Dynamic Programming/integer-break-343.ts
+++ Dynamic Programming/integer-break-343.ts
@@ -1,26 +1,42 @@
 // https://leetcode.com/problems/integer-break/
 
 // TL;DR:
-// Use a DP bottom-up approach
-//   - Initialize a DP array with the length of the target + 1, values = idx
-//   - Set the last value of the DP array to 0 (to ensure we can't use the last value since we have to split at least in 2 elements)
-//   - Iterate through the DP array from 2 to n and for each target:
-//     - Iterate through the target from 1 to target - 1 and for each i:
-//       - Update the target DP value with the max(current target DP value, i * the DP value of the target - i)
-//   - Return the last value of the DP array
+// Use a recursive approach with memoization (top-down)
+//   - Initialize a DP hash map with the target number as the key and the value as the maximum product
+//   - Base case: If the target is 1, return 1
+//   - Recursive case:
+//     - If the current target is already in the DP hash map, return the value
+//     - Initialize the product with 0 if it's the last element, otherwise the target (since we can't break the last element)
+//     - Iterate through the target from 1 to target - 1 and for each j:
+//       - Calculate the result by multiplying the product of the current target and the product of the target - j
+//       - Update the product with the maximum value
+//     - Update the DP hash map with the product
+//     - Return the product
 
 // Complexities:
 // Time => O(n^2), where n is the target number
 // Space => O(n), where n is the target number
 
 function integerBreak(n: number): number {
-	const dp = Array.from({ length: n + 1 }, (_, i) => i);
-	dp[n] = 0;
-	for (let target = 2; target <= n; target++) {
-		for (let i = 1; i < target; i++) {
-			dp[target] = Math.max(dp[target], i * dp[target - i]);
+	const dp: Record<number, number> = {};
+
+	function bkt(target: number): number {
+		if (target === 1) {
+			return 1;
 		}
+		if (dp[target] !== undefined) {
+			return dp[target];
+		}
+
+		let product = target === n ? 0 : target;
+		for (let j = 1; j < target; j++) {
+			const result = bkt(j) * bkt(target - j);
+			product = Math.max(product, result);
+		}
+
+		dp[target] = product;
+		return product;
 	}
 
-	return dp[n];
+	return bkt(n);
 }
