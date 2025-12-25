cfd81f85029e36a6e6de26237eafdb27a44d9ae6
antoniofalcescu <antonio.falcescu@gmail.com>
2025-12-25 08:16:08 +0200

    updated Single Threaded CPU 1834

--- Priority Queue/single-threaded-CPU-1834.ts
+++ Priority Queue/single-threaded-CPU-1834.ts
@@ -1,48 +1,55 @@
 // https://leetcode.com/problems/single-threaded-cpu/
 
 // TL;DR:
-// Create a sorted array of tasks with their indices (sorted based on enqueue time)
-// Use a min heap to store the tasks based on the processing time (if processing time is the same, based on the index)
-// Keep track of the current processed task index and the time
-// While we still have tasks that need to be processed (processed < n or tasks are still in the min heap)
-//   - While we have tasks to process from the main tasks array, push all tasks with enqueue time <= current time to the min heap and increment processed
-//   - If the heap is still empty, means we reached an idle lock, so we can jump directly to the next task by updating the current time to the enqueue time of the next task to process
-//   - If the min heap is not empty, pop the front task, add the index to the answer and increment the current time with the processing time
+// Use 2 Min Heaps approach to handle the possible and available tasks:
+//   - Use a min heap to store the tasks based on the enqueue time
+//   - Use a min heap to store the tasks based on the processing time (if processing time is the same, based on the index)
+// Keep track of the current time and the answer array
+// While we still have tasks that need to be processed (either possible or available tasks are still in the heaps)
+//   - While we have tasks to process from the possible heap, push all tasks with enqueue time <= current time to the available heap
+//   - If the available heap is still empty, means we reached an idle lock, so we can jump directly to the next task by updating the current time to the enqueue time of the next task to process
+//   - If the available heap is not empty, pop the front task, add the index to the answer array and increment the current time with the processing time
+// Return the answer array
 
 // Complexities:
 // Time => O(n * log(n)), where n is the number of tasks
 // Space => O(n), where n is the number of tasks
 
 function getOrder(tasks: number[][]): number[] {
-	const n = tasks.length;
-	const tasksWithIndex = tasks.map((t, i) => [...t, i]);
-	tasksWithIndex.sort((t1, t2) => t1[0] - t2[0]);
-
-	const minHeap = new PriorityQueue<number[]>((t1, t2) => {
-		if (t1[1] === t2[1]) {
-			return t1[2] - t2[2];
+	const tasksWithIdx = tasks.map((task, i) => [...task, i]) as [
+		number,
+		number,
+		number
+	][];
+	const possible = new MinPriorityQueue<[number, number, number]>(
+		(elem) => elem[0]
+	);
+	for (const task of tasksWithIdx) {
+		possible.enqueue(task);
+	}
+	const available = new PriorityQueue<[number, number, number]>((a, b) => {
+		if (a[1] === b[1]) {
+			return a[2] - b[2];
 		}
-
-		return t1[1] - t2[1];
+		return a[1] - b[1];
 	});
 
-	let processed = 0;
-	let time = tasksWithIndex[processed][0];
+	let time = possible.front()[0];
 	const ans = [];
-	while (processed < n || !minHeap.isEmpty()) {
-		while (processed < n && time >= tasksWithIndex[processed][0]) {
-			minHeap.enqueue(tasksWithIndex[processed]);
-			processed++;
+	while (!possible.isEmpty() || !available.isEmpty()) {
+		while (!possible.isEmpty() && possible.front()[0] <= time) {
+			const task = possible.dequeue();
+			available.enqueue(task);
 		}
 
-		if (minHeap.isEmpty()) {
-			time = tasksWithIndex[processed][0];
+		if (available.isEmpty()) {
+			time = possible.front()[0];
 			continue;
 		}
 
-		const [enqueue, processing, idx] = minHeap.dequeue();
+		const [_, process, idx] = available.dequeue();
+		time += process;
 		ans.push(idx);
-		time += processing;
 	}
 
 	return ans;
