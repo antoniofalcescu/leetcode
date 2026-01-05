013a9a1e4bfbd343d33dbeffd74ca7f9f6f28261
antoniofalcescu <antonio.falcescu@gmail.com>
2026-01-05 09:02:25 +0200

    updated Stone game 877

--- Dynamic Programming/stone-game-877.ts
+++ Dynamic Programming/stone-game-877.ts
@@ -7,8 +7,10 @@
 //   - Recursive case:
 //     - If the current value is already calculated, return the value
 //     - Calculate if alice picks by checking if the current index is even or odd (right - left) % 2 === 0 (alice starts and the array is even at the start)
-//     - Calculate the pick first and pick last by checking if the alice picks is true or false
-//     - Update the DP array with the maximum of the two possible picks (pick first + bkt(left + 1, right) or pick last + bkt(left, right - 1))
+//     - According to who picks:
+//       - If Alice picks, initialize the result with 0 and check both first/last stone picks and update the result with the maximum of the two
+//       - If Bob picks, initialize the result with Infinity and check both first/last stone picks and update the result with the minimum of the two (don't add the piles value to the result)
+//     - Update the DP array with the result
 // Calculate the total sum of the piles and return if alice's score is greater than the total sum minus alice's score
 
 // Complexities:
@@ -28,18 +30,23 @@ function stoneGame(piles: number[]): boolean {
 			return dp[left][right];
 		}
 
-		const alicePicks = (right - left) % 2 === 0;
-		const pickFirst = alicePicks ? piles[left] : 0;
-		const pickLast = alicePicks ? piles[right] : 0;
+		const alicePicks = (right - left + 1) % 2 === 0;
+		let result = alicePicks ? 0 : Infinity;
+		if (alicePicks) {
+			result = Math.max(
+				result,
+				piles[left] + bkt(left + 1, right),
+				piles[right] + bkt(left, right - 1)
+			);
+		} else {
+			result = Math.min(result, bkt(left + 1, right), bkt(left, right - 1));
+		}
 
-		dp[left][right] = Math.max(
-			pickFirst + bkt(left + 1, right),
-			pickLast + bkt(left, right - 1)
-		);
-		return dp[left][right];
+		dp[left][right] = result;
+		return result;
 	}
 
-	const total = piles.reduce((acc, num) => num + acc, 0);
-	const alice = bkt(0, piles.length - 1);
-	return alice > total - alice;
+	const aliceSum = bkt(0, piles.length - 1);
+	const totalSum = piles.reduce((acc, num) => acc + num, 0);
+	return aliceSum > totalSum - aliceSum;
 }
