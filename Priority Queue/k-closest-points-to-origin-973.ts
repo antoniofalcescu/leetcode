96f94d637b0de7396cdbde8274ade0c5d1ba0173
antoniofalcescu <antonio.falcescu@gmail.com>
2026-01-11 08:09:24 +0200

    updated K-Closest Points to Origin 973

--- Priority Queue/k-closest-points-to-origin-973.ts
+++ Priority Queue/k-closest-points-to-origin-973.ts
@@ -1,28 +1,26 @@
 // https://leetcode.com/problems/k-closest-points-to-origin/
 
 // TL;DR:
-// Use a min heap to store tuples of type [distance, point]
-// For each point, calculate the distance from the origin and push the tuple to the min heap
-// Pop the k points from the min heap and return them as an array of points
+// Use a max heap to store the points based on the distance from the origin
+// Iterate through the points and enqueue them to the heap while popping the farthest if we overflow over k
+// Return the heap as an array
 
 // Complexities:
-// Time => O(k * log(n)), where n is the number of points and k is the number of points to return
-// Space => O(n), where n is the number of points
+// Time => O(n * log(k)), where n is the number of points and k is the number of points to return
+// Space => O(k), where k is the number of points to return
 
-import { MinPriorityQueue } from "@datastructures-js/priority-queue";
+import { MaxPriorityQueue } from "@datastructures-js/priority-queue";
 
 function kClosest(points: number[][], k: number): number[][] {
-	const minHeap = new MinPriorityQueue<[number, number[]]>((elem) => elem[0]);
+	const maxHeap = new MaxPriorityQueue<number[]>((elem) =>
+		Math.sqrt(Math.pow(elem[0], 2) + Math.pow(elem[1], 2))
+	);
 	for (const point of points) {
-		const distance = Math.sqrt(Math.pow(point[0], 2) + Math.pow(point[1], 2));
-		minHeap.enqueue([distance, point]);
+		maxHeap.enqueue(point);
+		if (maxHeap.size() > k) {
+			maxHeap.dequeue();
+		}
 	}
 
-	const ans: number[][] = [];
-	for (let i = 0; i < k; i++) {
-		const [_, point] = minHeap.dequeue();
-		ans.push(point);
-	}
-
-	return ans;
+	return maxHeap.toArray();
 }
